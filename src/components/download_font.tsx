0a271c42b5314dd7264bb00ec1e2243ac8a9fc59
Enri9911 <vacondioenrico@gmail.com>
2022-04-10 19:06:25 +0200

    resources update

--- src/components/download_font.tsx
+++ src/components/download_font.tsx
@@ -5,23 +5,23 @@ const DownloadFont: React.FC = () => {
 
     return (
         <div className="flex flex-col">
-            <a href="/static/iconfont/iconmoon.ttf" download className="no-underline bg-gray-300 mt-4 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+            <a href="static/iconfont/iconmoon.ttf" download className="no-underline mt-4 font-bold py-2 px-4 rounded inline-flex items-center">
                 <span className="icon-file-download text-center"/>
                 <span className="ml-4 mr-4">iconmoon.ttf</span>
             </a>
-            <a download className="no-underline bg-gray-300 mt-4 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+            <a download className="no-underline  mt-4 font-bold py-2 px-4 rounded inline-flex items-center">
                 <span className="icon-file-download text-center"/>
                 <span className="ml-4 mr-4">iconmoon.woff</span>
             </a>
-            <a download className="bg-gray-300 mt-4 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+            <a download className="mt-4 font-bold py-2 px-4 rounded inline-flex items-center">
                 <span className="icon-file-download text-center"/>
                 <span className="ml-4 mr-4">iconmoon.eot</span>
             </a>
-            <a download className="bg-gray-300 mt-4 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+            <a download className="mt-4 font-bold py-2 px-4 rounded inline-flex items-center">
                 <span className="icon-file-download text-center"/>
                 <span className="ml-4 mr-4">font-style.css</span>
             </a>
-            <a download className="bg-gray-300 mt-4 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+            <a download className="mt-4 font-bold py-2 px-4 rounded inline-flex items-center">
                 <span className="icon-file-download text-center"/>
                 <span className="ml-4 mr-4">selection.json</span>
             </a>
